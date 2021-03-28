ddd76e004d0f73738985e2f618bf2014b1976e3e
VirtualEstatePlanner <georgegeorgulas@gmail.com>
2021-03-28 16:57:51 -0400

    no more type errors

--- src/index.ts
+++ src/index.ts
@@ -1,119 +1,106 @@
-import path from 'path'
 import fs from 'fs'
-import toml from 'toml'
+import path from 'path'
+import * as IGAPI from 'instagram-private-api'
 import _ from 'lodash'
-import mkdirp from 'mkdirp'
 import download from 'download'
-import { IgApiClient, UserStoryFeedResponseItemsItem } from 'instagram-private-api'
+import mkdirp from 'mkdirp'
+import toml from 'toml'
 import { IScraperConfig } from './interfaces/IScraperConfig'
-import { IImageDimensions } from './interfaces/IImageDimensions'
 
-const tomlConfigPath: string = fs.readFileSync(process.env.CONFIG_PATH || 'config.toml').toString()
-const runtimeConfig: IScraperConfig = toml.parse(tomlConfigPath)
+const tomlConfig: string = fs.readFileSync('config.toml').toString()
+const runtimeConfig: IScraperConfig = toml.parse(tomlConfig)
+const runAgainNext = 'next_execution_timestamp'
 
-class InstagramManagement {
-  async initialize() {
-    await this.instagram.simulate.preLoginFlow()
-    this.currentUser = await this.instagram.account.login(runtimeConfig.Instagram.Username, runtimeConfig.Instagram.Password)
-    console.log(`Logged in to Instagram as : ${this.currentUser}`)
-  }
+let instagramClient: IGAPI.IgApiClient = new IGAPI.IgApiClient()
+instagramClient.simulate.preLoginFlow()
+instagramClient.state.generateDevice(runtimeConfig.Instagram.Username)
+instagramClient.account.login(runtimeConfig.Instagram.Username, runtimeConfig.Instagram.Password)
+console.log(`Logged in to Instagram as : ${runtimeConfig.Instagram.Username}`)
 
-  instagram: IgApiClient
-  currentUser: any
-  constructor() {
-    this.instagram = new IgApiClient()
-    this.instagram.state.generateDevice(runtimeConfig.Instagram.Username)
-
-    this.currentUser = null
-  }
-
-  async saveFile(fileURL: string, baseFolder: string | undefined) {
-    return download(fileURL, baseFolder)
-  }
-
-  async findBestQuality(listOfVersions: UserStoryFeedResponseItemsItem[]) {
-    return _.first(
-      listOfVersions.sort((version: IImageDimensions) => {
-        return version.height * version.width
-      })
-    )
-  }
-
-  async saveStory(storyData: UserStoryFeedResponseItemsItem, baseFolder: any) {
-    switch (storyData.media_type) {
-      case 1:
-        await this.saveFile(this.findBestQuality(storyData.image_versions2.candidates), baseFolder)
-        break
-      case 2:
-        await this.saveFile(this.findBestQuality(storyData.video_versions), baseFolder)
-        break
-      default:
-        throw new Error(`SaveStory method: hit default case in story.Datamedia_type switch`)
-        break
-    }
-  }
-
-  async scrapeStories(username: string, baseFolder: string) {
-    await mkdirp(`${baseFolder}/${username}`)
-    const storyFeed = this.instagram.feed.userStory(await this.instagram.user.getIdByUsername(username))
-    await storyFeed.request()
-
-    let resolve: Function
-    const r = new Promise((rz) => {
-      resolve = rz
-    })
-    storyFeed.items$.subscribe(
-      (storyList) => {
-        return storyList.map((s) => this.saveStory(s, baseFolder))
-      },
-      (error) => {
-        throw new Error(error)
-      },
-      () => {
-        console.log('Done scraping.')
-        resolve()
+const findBestQuality: Function = async (listOfVersions: IGAPI.UserStoryFeedResponseItemsItem[]) => {
+  return _.first(
+    listOfVersions.sort((version: IGAPI.UserStoryFeedResponseItemsItem, version2: IGAPI.UserStoryFeedResponseItemsItem) => {
+      if (version.original_height * version.original_width > version2.original_height * version2.original_width) {
+        return 1
       }
-    )
+      if (version.original_height * version.original_width < version2.original_height * version2.original_width) {
+        return -1
+      }
+      return 0
+    })
+  )
+}
 
-    return r
+const saveFile: Function = async (fileURL: string, baseFolder: string | undefined) => {
+  return download(fileURL, baseFolder)
+}
+
+const saveStory: Function = async (storyData: IGAPI.UserStoryFeedResponseItemsItem, baseFolder: any) => {
+  switch (storyData.media_type) {
+    case 1:
+      saveFile(findBestQuality(storyData.image_versions2.candidates), baseFolder)
+      break
+    case 2:
+      saveFile(findBestQuality(storyData.video_versions), baseFolder)
+      break
+    default:
+      throw new Error(`Error in saveStory: hit default case in storyData.media_type switch`)
   }
 }
 
-const runAgainNext = runtimeConfig.TimestampFile || 'next_execution_timestamp'
+const scrapeStories: Function = async (username: string, baseFolder: string) => {
+  await mkdirp(`${baseFolder}/${username}`)
+  const storyFeed = instagramClient.feed.userStory(await instagramClient.user.getIdByUsername(username))
+  await storyFeed.request()
 
-async function executeScrape(time: number) {
-  const timeNow = new Date()
-  await Promise.all(
-    runtimeConfig.Targets.map(async (scrapedUser: string) => {
-      console.log(`Started scraping ${scrapedUser}`)
-
-      let folderPth: string
-      if (runtimeConfig.FolderFormat === 1) {
-        folderPth = path.join(runtimeConfig.BaseFolder, `${timeNow.getFullYear()}-${timeNow.getMonth() + 1}`, `${timeNow.getDate().toString()}/`, scrapedUser)
-      } else if (runtimeConfig.FolderFormat === 2) {
-        folderPth = path.join(runtimeConfig.BaseFolder, scrapedUser, `${timeNow.getFullYear()}-${timeNow.getMonth() + 1}`, `${timeNow.getDate().toString()}/`)
-      } else {
-        throw new Error(`Execute Scrape Error: Invalid Folder Format found: ${runtimeConfig.FolderFormat}`)
-      }
-      InstagramManagement.scrapeStories(scrapedUser, folderPth)
-      console.log(`finished scraping user ${scrapedUser}`)
-    })
+  let resolve: Function
+  const scrapePromise = new Promise((resolution) => (resolve = resolution))
+  storyFeed.items$.subscribe(
+    (storyList: IGAPI.UserStoryFeedResponseItemsItem[]) => {
+      return storyList.map((story) => saveStory(story, baseFolder))
+    },
+    (error: string) => {
+      throw new Error(`Error in scrapeStory: ${error}`)
+    },
+    () => {
+      resolve()
+      console.log('Done scraping.')
+    }
   )
+  return scrapePromise
+}
 
+const executeScrape: Function = async (time: number) => {
+  const timeNow = new Date()
   const nextTicker: number = time + 24 * 60 * 60 * 1000
   const buffer: Buffer = Buffer.alloc(8)
+  await Promise.all(
+    runtimeConfig.Targets.map(
+      async (eachTarget: string): Promise<void> => {
+        console.log(`Started scraping ${eachTarget}`)
+
+        let folderPath: string
+        if (runtimeConfig.FolderFormat === 1) {
+          folderPath = path.join(runtimeConfig.BaseFolder, `${timeNow.getFullYear()}-${timeNow.getMonth() + 1}`, `${timeNow.getDate().toString()}/`, eachTarget)
+        } else if (runtimeConfig.FolderFormat === 2) {
+          folderPath = path.join(runtimeConfig.BaseFolder, eachTarget, `${timeNow.getFullYear()}-${timeNow.getMonth() + 1}`, `${timeNow.getDate().toString()}/`)
+        } else {
+          throw new Error(`Execute Scrape Error: Invalid Folder Format found: ${runtimeConfig.FolderFormat}`)
+        }
+        scrapeStories(eachTarget, folderPath)
+        console.log(`finished scraping user ${eachTarget}`)
+      }
+    )
+  )
   buffer.writeDoubleLE(nextTicker)
   fs.writeFileSync(runAgainNext, buffer)
   setTimeout(() => executeScrape(nextTicker), nextTicker - Date.now())
 }
 
-async function main() {
-  InstagramManagement.initialize()
-
-  console.log(`Instagram Management instance initialized!`)
+const main: Function = async (): Promise<void> => {
   try {
     fs.accessSync(runAgainNext)
-  } catch (e) {
+  } catch (e: any) {
     await executeScrape(Date.now())
     return
   }
@@ -126,6 +113,4 @@ async function main() {
   setTimeout(() => executeScrape(nextTick), timeRemaining)
 }
 
-main().catch((e) => console.error(e))
-
-export const im = new InstagramManagement()
+main().catch((e: Error) => console.error(e))
